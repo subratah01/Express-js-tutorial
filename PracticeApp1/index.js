00e192c03005643ac72e8a37a1bfe0870e769143
subratah01 <subratah01@gmail.com>
2022-08-28 00:18:31 +0530

    Response redirect

--- PracticeApp1/index.js
+++ PracticeApp1/index.js
@@ -1,8 +1,37 @@
 var express=require('express');
 var app =express();
 
+//String Response
 app.get('/',function(req,res){
     res.send('Hello express js!');
+})
+
+//Response Status Code
+app.get('/one', function(req, res){
+    res.status(201).end('Object created!');
+})
+
+//JSON Response
+app.get('/two', function(req, res){
+    var jsonData={
+        'FirstName':'Subrata',
+        'LastName':'Haldar',
+    }
+    res.json(jsonData);
+})
+
+//Download Response
+app.get('/three', function(req, res){
+    res.download('./uploads/SearchLead.png');
+})
+
+//Response Redirect
+app.get('/bangladesh', function(req, res){
+    res.redirect("http://localhost:8000/india");
+})
+
+app.get('/india', function(req, res){
+    res.end('This is India');
 })
 
 
